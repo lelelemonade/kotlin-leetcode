dab934494e47b9fdaf11cb8f24c60a317af64d29
zhonglishen <szl921818@gmail.com>
2024-08-21 18:08:45 +0200

    project init

--- src/test/kotlin/org/carrot/kotlin_leetcode/Test70/Test70.kt
+++ src/test/kotlin/org/carrot/kotlin_leetcode/Test70/Test70.kt
@@ -4,8 +4,23 @@ import org.junit.jupiter.api.Test
 
 class Test70 {
 
+    fun climbStairs(n: Int): Int {
+        if (n<=2){
+            return n
+        }
+        val dp = IntArray(n) { 0 }
+        dp[0]=1
+        dp[1]=2
+
+        for (i in 2..<n){
+            dp[i]=dp[i-2]+dp[i-1]
+        }
+
+        return dp[n-1]
+    }
+
     @Test
     fun test70(){
-        println("Hello")
+        assert(climbStairs(3)==3)
     }
 }
